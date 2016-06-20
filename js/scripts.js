8b0b93d4e704a76c9db5fa9eef92469642a3aaf4
Cameron Poore and James Howard <cameron@>
2016-06-20 14:13:15 -0700

    Implemented if/else statement for currentLocation not falling off the map.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 var currentLocation = [1,1];
 //first index EW second NS
-var mapLocation = [
-    ["0,0 : Southwest corner of a quiet meadow.", "1,0 : South side of a quiet meadow.", "2,0 : Southeast corner of a quiet meadow."],
-    ["0,1 : West side of a quiet meadow.", "1,1 : Dead center of a quiet meadow.", "2,1 : East side of a quiet meadow."],
-    ["0,2 : Northwest corner of a quiet meadow.", "1,2 : North side of a quiet meadow.", "2,2 : Northeast corner of a quiet meadow."],
-
-];
-
 var moveLocation = function(direction) {
   var currentMapLocation = mapLocation[direction[1]][direction[0]];
   $("#main_output p").text(currentMapLocation);
+
+
+if (currentLocation > -1 || currentLocation > 4){
+  currentLocation = true;
+} else {
+  currentLocation = false;
+}
 
 
 
